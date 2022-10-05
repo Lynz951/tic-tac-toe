90ba7096f866fbdacc352c428a13dba3ebbb6c96
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-10-05 21:10:16 +0000

    tiny functions that actually work

--- main.js
+++ main.js
@@ -2,25 +2,32 @@
 
 //Variables
 
+let player = 'X';
+console.log(player);
+let boardState = ['', '', '', '', '', '', '', '', ''];
+console.log(boardState);
+let playerTurn = 1; 
+console.log(playerTurn);
+
 // var title = document.createElement("TITLE");
 // var t = document.createTextNode("Title");
 // title.appendChild(t);
 
 // document.getElementById("app").innerHTML = "Tic-Tac-Toe";
 
-const startButton = document.getElementById("start");
+// const startButton = document.getElementById("start");
 const resetButton = document.getElementById("reset");
 
 //Styling for div.app
 
 //Start button
-startButton.type = 'button';
-startButton.id = 'start';
-startButton.value = 'Start';
-startButton.className = 'btn';
-app.appendChild(start);
+// startButton.type = 'button';
+// startButton.id = 'start';
+// startButton.value = 'Start';
+// startButton.className = 'btn';
+// app.appendChild(start);
 
-startButton.onclick = drawBoard;
+// startButton.onclick = drawBoard;
 
 //Reset button
 
@@ -31,6 +38,27 @@ resetButton.className = 'btn';
 app.appendChild(reset);
 
 resetButton.onclick = init;
+
+//Functions for buttons
+
+function function0() {
+    console.log(player === 'X');
+    if (player === 'X') {
+    boardState.splice(0, 1, 'X');
+    playerTurn = playerTurn + 1;  
+    } else if (player === 'O') {
+        boardState.splice(0, 1, 'O');
+        playerTurn = playerTurn + 1;
+    }
+}
+
+
+// function function0() {
+//     if (boardState[0] = null) {
+//         let boardState = boardState[0].splice('', 'X');
+//     } 
+//     return newBoardState;
+// }
 
 // // init function
 // function init() {
@@ -67,10 +95,13 @@ resetButton.onclick = init;
 // }
 
 // //changePlayer function
-// function changePlayer() {
-
-// }
-
+function changePlayer () {
+    if (player === 'X') {
+        player = 'O';
+    } else if (player === 'O') {
+        player = 'X';
+    }      
+}
 // //gameResult function
 // function gameResult() {
 
