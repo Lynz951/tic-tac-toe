25d409a8f6421cba3bfc1e646af3a9906ff64a78
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-10-05 19:09:55 +0000

    got look

--- main.js
+++ main.js
@@ -1,86 +1,94 @@
-document.body.onload = init;
+//document.body.onload = init;
 
 //Variables
 
+// var title = document.createElement("TITLE");
+// var t = document.createTextNode("Title");
+// title.appendChild(t);
+
+// document.getElementById("app").innerHTML = "Tic-Tac-Toe";
+
+const startButton = document.getElementById("start");
+const resetButton = document.getElementById("reset");
 
 //Styling for div.app
 
 //Start button
-button.type = 'button';
-button.id = 'start';
-button.value = 'Start';
-button.className = 'stbtn';
-container.appendChild(startButton);
+startButton.type = 'button';
+startButton.id = 'start';
+startButton.value = 'Start';
+startButton.className = 'btn';
+app.appendChild(start);
 
-button.onclick = drawBoard;
+startButton.onclick = drawBoard;
 
 //Reset button
 
-button.type = 'button';
-button.id = 'reset';
-button.value = 'Reset';
-button.className = 'rstbtn';
-container.appendChild(resetButton);
+resetButton.type = 'button';
+resetButton.id = 'reset';
+resetButton.value = 'Reset';
+resetButton.className = 'btn';
+app.appendChild(reset);
 
-button.onclick = init;
+resetButton.onclick = init;
 
-// init function
-function init() {
-    var title = document.createElement("TITLE");
-    var t = document.createTextNode("Title");
-    title.appendChild(t);
+// // init function
+// function init() {
+//     var title = document.createElement("TITLE");
+//     var t = document.createTextNode("Title");
+//     title.appendChild(t);
 
-    document.getElementById("app").innerHTML = "Tic-Tac-Toe";
-}
+//     document.getElementById("app").innerHTML = "Tic-Tac-Toe";
+// }
 
-//drawBoard function
-function drawBoard() {
+// //drawBoard function
+// function drawBoard() {
 
-}
+// }
 
-//setState function
-function setState() {
+// //setState function
+// function setState() {
 
-}
+// }
 
-//handleClick function
-function handleClick() {
+// //handleClick function
+// function handleClick() {
 
-}
+// }
 
-//updateSpace function
-function updateSpace() {
+// //updateSpace function
+// function updateSpace() {
 
-}
+// }
 
-//checkWinConditions function
-function checkWinConditions() {
+// //checkWinConditions function
+// function checkWinConditions() {
 
-}
+// }
 
-//changePlayer function
-function changePlayer() {
+// //changePlayer function
+// function changePlayer() {
 
-}
+// }
 
-//gameResult function
-function gameResult() {
+// //gameResult function
+// function gameResult() {
 
-}
+// }
 
-//finishGame function
-function finishGame() {
+// //finishGame function
+// function finishGame() {
 
-}
+// }
 
-//resetGame function
-function resetGame() {
-    //clear DOM
-    //make all spaces clear and clickable
+// //resetGame function
+// function resetGame() {
+//     //clear DOM
+//     //make all spaces clear and clickable
 
-}
+// }
 
-//makeMove function
-function makeMove() {
+// //makeMove function
+// function makeMove() {
 
-}
+// }
